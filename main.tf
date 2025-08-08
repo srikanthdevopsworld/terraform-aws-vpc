c179e1e7638f46a64f2a86cca64601c9a7747d5f
Srikanth <srikanthdevopsworld@gmail.com>
2025-08-08 21:01:58 +0530

    latest

--- main.tf
+++ main.tf
@@ -163,19 +163,19 @@ resource "aws_route" "database" {
 }
 
 resource "aws_route_table_association" "public" {
-  count = length(var.public_subnet_cidrs)
+  count = length(var.public_subnet_cidr)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
-  count = length(var.private_subnet_cidrs)
+  count = length(var.private_subnet_cidr)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "database" {
-  count = length(var.database_subnet_cidrs)
+  count = length(var.database_subnet_cidr)
   subnet_id      = aws_subnet.database[count.index].id
   route_table_id = aws_route_table.database.id
 }
