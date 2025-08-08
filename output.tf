a09fa0f4e84fe6d423b3f8299582dda7ed59011a
Srikanth <srikanthdevopsworld@gmail.com>
2025-08-08 20:30:42 +0530

    latest

--- output.tf
+++ output.tf
@@ -6,3 +6,19 @@
 # output "subnets_info" {
 #   value = aws_subnet.public
 # }
+
+output "vpc_id" {
+  value = aws_vpc,main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet_public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet_private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet_database[*].id
+}
